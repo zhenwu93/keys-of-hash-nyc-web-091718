d3d562ed93f19a322a8c1f5aec2b8fbe162e89ba
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 06:24:03 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(arguments)
     arr = []
     arguments.each do |arg|
-      self.each do |key, value|
+      arg.each do |key, value|
         if arg == value
           arr.push(key)
         end
