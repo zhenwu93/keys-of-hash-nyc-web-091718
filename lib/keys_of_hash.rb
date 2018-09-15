1d1491fcc5a759570261a36c52d3621428502185
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-15 06:12:17 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    array = []
+    self.each do |key, value|
+      arguments.each do |i|
+        if i == value
+          array.push(key)
+        end
+      end
+    end
+    return array
   end
 end
